97cae97a6bd52eb53c55fe9f6e74d2f44b237393
Robert Guss <robert.guss@insuremytrip.com>
2020-08-25 11:01:33 -0700

    installed webpack dev server and html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,17 @@
 const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  devServer: {
+    contentBase: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+    }),
+  ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
